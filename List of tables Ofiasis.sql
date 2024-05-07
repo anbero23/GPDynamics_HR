130ac0a19f05e0ee025ebf0132b60d98d019c8b7
Andrea Becerra <abecerra2@student.hult.edu>
2024-05-06 23:25:32 -0700

    adding SQL code

---
+++ List of tables Ofiasis.sql
@@ -0,0 +1,6 @@
+--List of tables Ofiasis
+
+-- Marcas de paso, relojes insertan aca las marcas
+SELECT TOP(10) * FROM  OFIASIS.dbo.TWREGI_RELO ORDER BY FE_REGI_RELO DESC
+-- Marcas en OfiAsis
+SELECT TOP(10) * FROM  OFIASIS.dbo.TDREGI_RELO ORDER BY FE_REGI_RELO desc
